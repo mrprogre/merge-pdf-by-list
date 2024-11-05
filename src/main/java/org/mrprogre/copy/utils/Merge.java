3f20398e58458cdefa03d1b3d14a86288646f353
Dmitry <rps_project@mail.ru>
2024-11-05 10:51:13 +0300

    optimisation

--- src/main/java/org/mrprogre/copy/utils/Merge.java
+++ src/main/java/org/mrprogre/copy/utils/Merge.java
@@ -12,17 +12,16 @@ public class Merge {
     public static void mergePdfFiles(List<String> fileNamesToCopy, String outputFile) {
         PDFMergerUtility merger = new PDFMergerUtility();
 
-        for (String sourceFile : fileNamesToCopy) {
-            try {
+        try {
+            for (String sourceFile : fileNamesToCopy) {
                 merger.addSource(new File(sourceFile));
-                merger.setDestinationFileName(outputFile);
-
-                // Выполняем объединение
-                merger.mergeDocuments(null);
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
             }
+            merger.setDestinationFileName(outputFile);
+
+            // Выполняем объединение один раз
+            merger.mergeDocuments(null);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -45,10 +44,10 @@ public class Merge {
         if (files != null) {
             for (Record record : fileNamesToCopy) {
                 String name = Common.removeSpecialCharacters(record.getName());
+                int quantity = record.getQuantity();
 
                 for (File file : files) {
                     String recordName = Common.removeSpecialCharacters(file.getName());
-                    int quantity = record.getQuantity();
 
                     if (recordName.contains(name)) {
                         if (!file.exists() || quantity < 1) {
@@ -56,16 +55,17 @@ public class Merge {
                         }
 
                         for (int i = 1; i <= quantity; i++) {
-                            System.out.println(sourceDir + "\\" + file.getName());
-                            pdfList.add(sourceDir + "\\" + file.getName());
+                            System.out.println(sourceDir + File.separator + file.getName());
+                            pdfList.add(sourceDir + File.separator + file.getName());
                         }
 
                         counter++;
                     }
-
                 }
-                mergePdfFiles(pdfList, targetDir + "\\all_pdf_files.pdf");
             }
+
+            // Объединяем файлы после завершения добавления
+            mergePdfFiles(pdfList, targetDir + File.separator + "all_pdf_files.pdf");
 
             return "merged files " + counter + " of " + fileNamesToCopy.size() +
                     " (" + (int) (((double) counter / fileNamesToCopy.size()) * 100) + " %)";
